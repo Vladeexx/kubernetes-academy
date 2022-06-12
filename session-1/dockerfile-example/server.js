5bc17b9f763c6cef761ae1af0e11b60747704b9d
Vladimir Pavicic <vladimir.pavicic@sentia.com>
2022-06-12 09:21:21 +0200

    play with app

--- session-1/dockerfile-example/server.js
+++ session-1/dockerfile-example/server.js
@@ -7,7 +7,7 @@ const port = 3001;
 
 app.get('/', async function(req, res){
     res.json({
-        message: "this is Vladex first app on container hip hip hurayyy!"
+        message: "this is Vladex second app on container hip hip hurayyy!"
     });
 });
 
